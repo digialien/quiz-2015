6d9bfdd658358714e53a630b984b926aab4a0097
Qin Jian <qin.jian@outlook.com>
2015-08-16 17:26:51 +0200

    added loginRequired for stats

--- routes/index.js
+++ routes/index.js
@@ -36,7 +36,7 @@ router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',
 
 router.get('/timeout',                     quizController.transactionTimeout);
 
-router.get('/statistics',                  quizController.getStats);
+router.get('/statistics',                  sessionController.loginRequired, quizController.getStats);
 
 module.exports = router;
 
